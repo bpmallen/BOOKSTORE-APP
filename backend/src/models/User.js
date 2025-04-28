2c3c96139ffe510c457e1fed64dbe44181fd632d
bpmallen <bpmallen@gmx.com>
2025-04-28 13:27:50 +0100

    4 - login route & userSchema timestamps

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // hash password before saving user to database
 userSchema.pre("save", async function (next) {
@@ -32,6 +37,12 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
+
+// Compare password function
+
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
